b9106e521b04d0fd64ca599cb1101fc76f207d78
deconstructionalism <deconstructionalism@gmail.com>
2018-12-02 09:35:02 -0500

    Cleaned up handleSheetData function.

--- bin/summarize.js
+++ bin/summarize.js
@@ -6,14 +6,10 @@ const courseTrackerId = '1FtwouNfcAWYmU6Efq6MBN0CqYYLtoF_sd8RlnCYdxtQ'
 
 const handleSheetData = (client, outFile) => {
   const { sheetCollections } = client
-  const sheetCollection = sheetCollections[courseTrackerId]
-  const feedback = new StudentFeedback(sheetCollection)
-  const classRoom = feedback.classRoom
-  const roster = feedback.roster
-  const allData = JSON.stringify({
-    classRoom,
-    roster
-  }, null, 4)
+  const courseTracker = sheetCollections[courseTrackerId]
+  const feedback = new StudentFeedback(courseTracker)
+  const { classRoom, roster } = feedback
+  const allData = JSON.stringify({ classRoom, roster }, null, 4)
   console.log(allData)
   if (outFile) {
     fs.writeFileSync(outFile, allData)
